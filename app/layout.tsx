ad4bb20c3a6c5ce61cfcd62474c9da7b118a2faa
WangTZ1996 <wangtianzhu01@huohua.cn>
2023-12-11 22:56:49 +0800

    feat: update seo keys

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title:
-    "临时邮箱、邮箱、电子邮件、email、一次性临时电子邮件服务，随机临时邮箱、十分钟邮箱（10分钟)、临时邮、临时Email、快速注册Email、测试邮箱 -- 王天柱的blog",
+    "临时邮箱 Temp Email -- 王天柱的blog",
   description:
-    "在线生成临时邮箱、十分钟邮箱（10分钟)、快速注册Email、临时邮箱提供临时、匿名、免费、安全、一次性的电子邮件地址，保护您的个人电子邮件地址免受垃圾邮件的侵害。",
+    "在线生成临时邮箱（10分钟)，保护您的个人电子邮件地址免受垃圾邮件的侵害。",
+  keywords: "邮箱、电子邮件、email、一次性临时电子邮件服务，随机临时邮箱、十分钟邮箱（10分钟)、临时邮、临时Email、快速注册Email、测试邮箱"
 };
 
 export default function RootLayout({
